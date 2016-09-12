df4f5f9bb9d877c8c671a1ef93e9369574e3d387
hannazclass <hannazclass@gmail.com>
2016-09-12 16:27:45 +0900

    Add files via upload

---
+++ week1/Comment.java
@@ -0,0 +1,19 @@
+package com.hb.java.week1;
+
+public class Comment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 주석은 실행이 되지 않습니다.
+		 * 블라블라블라
+		 */
+		//단일 주석 단축키 ctrl+/
+		//문장 주석 단축키 ctrl+shift+/,ctrl+shift+\ 
+		System.out.println("김한나");
+		System.out.println("김한나");
+		System.out.println("김한나");
+		
+	}
+
+}
